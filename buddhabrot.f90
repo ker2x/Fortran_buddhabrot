4091fa04fe2135609dd5797ab3fc968e06542fff
Laurent Laborde <kerdezixe@gmail.com>
2010-03-11 21:48:19 +0100

    parameter change

--- buddhabrot.f90
+++ buddhabrot.f90
@@ -6,10 +6,10 @@ CHARACTER ( len = 255 ), PARAMETER :: filename = 'buddhabrot.ppm'
 INTEGER, PARAMETER :: file_out_unit = 10
  
  
-INTEGER, PARAMETER :: n_max=10000
+INTEGER, PARAMETER :: n_max=100
 INTEGER, PARAMETER :: grid_resolution = 512
 INTEGER, PARAMETER :: zpower = 2
-INTEGER*8, PARAMETER :: batchSize = 1000000
+INTEGER*8, PARAMETER :: batchSize = 10000000
 REAL, PARAMETER :: escapeOrbit = 2
 REAL, PARAMETER :: xmin = -1.0, xmax = 2.0, ymin = -1.3, ymax =1.3
  
@@ -38,9 +38,9 @@ INTEGER :: ppm_jhi
 INTEGER :: ppm_jlo
  
 !initialize exposureMap to 1
-exposureRMap = 1
-exposureGMap = 1
-exposureBMap = 1
+exposureRMap = 0
+exposureGMap = 0
+exposureBMap = 0
  
 !$OMP PARALLEL DO DEFAULT(PRIVATE),SHARED(exposureRMap, exposureGMap, exposureBMap)
 DO i=1, batchSize
@@ -57,15 +57,15 @@ DO i=1, batchSize
         TempY = INT(grid_resolution * (AIMAG(z) + ymax) / (ymax - ymin))
         TempYm = INT(grid_resolution/2 - (TempY - grid_resolution/2))
         IF((TempX > 0) .AND. (TempX < grid_resolution) .AND. (TempY > 0) .AND. (TempY < grid_resolution)) THEN
-          IF((iter > 2) .AND. (iter < 1000)) THEN
+          IF((iter > 2) .AND. (iter < 10)) THEN
             exposureRMap(TempX, TempY)  = exposureRMap(TempX, TempY) + 1
             exposureRMap(TempX, TempYm) = exposureRMap(TempX, TempYm) + 1
           END IF
-          IF((iter > 1000) .AND. (iter < 5000)) THEN
+          IF((iter > 10) .AND. (iter < 50)) THEN
             exposureGMap(TempX, TempY)  = exposureGMap(TempX, TempY) + 1
             exposureGMap(TempX, TempYm) = exposureGMap(TempX, TempYm) + 1
           END IF
-          IF((iter > 5000) .AND. (iter < 10000)) THEN
+          IF((iter > 50) .AND. (iter < 100)) THEN
             exposureBMap(TempX, TempY)  = exposureBMap(TempX, TempY) + 1
             exposureBMap(TempX, TempYm) = exposureBMap(TempX, TempYm) + 1
           ENDIF
